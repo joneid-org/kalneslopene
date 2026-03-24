d38fb597444892b99f304db017d007d0bb9d7a39
Emilie <eilie.nilsen@vegvesen.no>
2026-03-24 21:42:32 +0100

    ryddet opp i db filer

---
+++ backend/src/main/resources/db/changelog/sql/mockdata/seed_organizers.sql
@@ -0,0 +1,42 @@
+INSERT INTO organizer (uuid, name, responsibility, initials, phone, email) VALUES
+(
+    'd1000000-0000-0000-0000-000000000001',
+    'Emilie Nilsen',
+    ARRAY['Leder', 'Påmelding', 'Kommunikasjon'],
+    'EN',
+    '97654321',
+    'emilie@kalneslopene.no'
+),
+(
+    'd1000000-0000-0000-0000-000000000002',
+    'Magnus Holm',
+    ARRAY['Tidtaking', 'Resultater'],
+    'MH',
+    '91234567',
+    'magnus@kalneslopene.no'
+),
+(
+    'd1000000-0000-0000-0000-000000000003',
+    'Turid Bakken',
+    ARRAY['Løypemerking', 'Sikkerhet'],
+    'TB',
+    '98765432',
+    NULL
+),
+(
+    'd1000000-0000-0000-0000-000000000004',
+    'Rune Sørensen',
+    ARRAY['Utstyr', 'Teknisk ansvarlig'],
+    'RS',
+    '90123456',
+    'rune@kalneslopene.no'
+),
+(
+    'd1000000-0000-0000-0000-000000000005',
+    'Astrid Lien',
+    ARRAY['Sosiale medier', 'Fotografering'],
+    'AL',
+    NULL,
+    'astrid@kalneslopene.no'
+);
+

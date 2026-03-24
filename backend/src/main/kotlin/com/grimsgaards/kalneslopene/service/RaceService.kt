849b4755fb57ac70c917b939c6a3387971fdc7f3
Emilie <eilie.nilsen@vegvesen.no>
2026-03-24 21:53:17 +0100

    Getmappings

---
+++ backend/src/main/kotlin/com/grimsgaards/kalneslopene/service/RaceService.kt
@@ -0,0 +1,30 @@
+package com.grimsgaards.kalneslopene.service
+
+import com.grimsgaards.kalneslopene.model.dto.RaceDTO
+import com.grimsgaards.kalneslopene.repository.RaceRepository
+import org.springframework.stereotype.Service
+import java.time.LocalDate
+import java.util.*
+
+@Service
+class RaceService(
+    val raceRepository: RaceRepository
+) {
+    fun getUpcomingRaces(): List<RaceDTO> {
+        return raceRepository.findAllByRaceDateIsAfter(LocalDate.now()).sortedByDescending { it.raceDate }
+            .map { it.toDto() }
+    }
+
+    fun getPreviousRaces(): List<RaceDTO> {
+        return raceRepository.findAllByRaceDateIsLessThanEqual(LocalDate.now()).sortedByDescending { it.raceDate }
+            .map { it.toDto() }
+    }
+
+    fun getAll(): List<RaceDTO> {
+        return raceRepository.findAll().sortedByDescending { it.raceDate }.map { it.toDto() }
+    }
+
+    fun findByUuid(uuid: UUID): RaceDTO {
+        return raceRepository.findByUuid(uuid).toDto()
+    }
+}

531fae34b1592d7291b387f75a06cd8eca5ee4a5
Emilie <eilie.nilsen@vegvesen.no>
2026-03-26 16:08:09 +0100

    Create endpoints for runner.

---
+++ backend/src/main/kotlin/com/grimsgaards/kalneslopene/controller/RunnerController.kt
@@ -0,0 +1,49 @@
+package com.grimsgaards.kalneslopene.controller
+
+import com.grimsgaards.kalneslopene.model.dto.RunnerDTO
+import com.grimsgaards.kalneslopene.service.RunnerService
+import org.springframework.web.bind.annotation.*
+import java.util.*
+
+@RestController
+@RequestMapping("/api/runner")
+class RunnerController(
+    val runnerService: RunnerService
+) {
+
+    @GetMapping
+    fun getAllRunners(): List<RunnerDTO> {
+        return runnerService.getAllRunners()
+    }
+
+    @GetMapping("/name/{name}")
+    fun getRunnerByName(@PathVariable name: String): List<RunnerDTO> {
+        return runnerService.getRunnerByName(name)
+    }
+
+    @GetMapping("/{uuid}")
+    fun getRunnerByUuid(@PathVariable uuid: UUID): RunnerDTO {
+        return runnerService.getRunnerById(uuid)
+    }
+
+    @PatchMapping("")
+    fun updateRunner(@RequestBody runner: RunnerDTO): RunnerDTO {
+        return runnerService.updateRunner(runner)
+    }
+
+    @PostMapping("")
+    fun createRunner(@RequestBody runner: RunnerDTO): RunnerDTO {
+        return runnerService.createRunner(runner)
+    }
+
+    @PostMapping("/createMultipleRunners")
+    fun createMultipleRunners(@RequestBody runners: List<RunnerDTO>): List<RunnerDTO> {
+        return runnerService.createMultipleRunners(runners)
+    }
+
+    @DeleteMapping("/{uuid}")
+    fun deleteRunner(@PathVariable uuid: UUID) {
+        runnerService.deleteRunner(uuid)
+    }
+
+}
---
+++ backend/src/main/kotlin/com/grimsgaards/kalneslopene/model/entities/RunnerEntity.kt
@@ -0,0 +1,20 @@
+package com.grimsgaards.kalneslopene.model.entities
+
+import com.grimsgaards.kalneslopene.model.dto.RunnerDTO
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+import java.util.*
+
+@Entity
+@Table(name = "runner")
+data class RunnerEntity(
+    @Id
+    val uuid: UUID = UUID.randomUUID(),
+    val name: String,
+    val gender: String
+) {
+    fun toDto(): RunnerDTO {
+        return RunnerDTO(uuid, name, gender)
+    }
+}

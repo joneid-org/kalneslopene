a8413f40dc3410c477e795922da1b189c39c9c3a
ejoneid <eirijomine@hotmail.com>
2026-07-19 13:16:35 +0200

    chore: update infra for prod deployment

--- infra/servers.tf
+++ infra/servers.tf
@@ -3,8 +3,8 @@ resource "hcloud_server" "dokploy" {
   image              = var.os_type
   server_type        = "cx23"
   location           = var.location
-  delete_protection  = false
-  rebuild_protection = false
+  delete_protection  = true
+  rebuild_protection = true
   ssh_keys           = [for k in hcloud_ssh_key.team : k.id]
   user_data = templatefile("user_data.yml.tftpl", {
     ssh_public_key = file(pathexpand("~/.ssh/ejoneid_devops.pub"))

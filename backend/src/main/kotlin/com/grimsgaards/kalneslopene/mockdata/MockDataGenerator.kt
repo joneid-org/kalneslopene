32a019a7728aec41b0842f9319c9740a86567279
ejoneid <eirijomine@hotmail.com>
2026-07-20 23:28:09 +0200

    chore: fix backend build

--- backend/src/main/kotlin/com/grimsgaards/kalneslopene/mockdata/MockDataGenerator.kt
+++ backend/src/main/kotlin/com/grimsgaards/kalneslopene/mockdata/MockDataGenerator.kt
@@ -18,7 +18,6 @@ import com.grimsgaards.kalneslopene.repository.RaceRunnerRepository
 import com.grimsgaards.kalneslopene.repository.RunnerRepository
 import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
-import org.springframework.context.annotation.Profile
 import org.springframework.stereotype.Component
 import org.springframework.transaction.annotation.Transactional
 import java.time.DayOfWeek

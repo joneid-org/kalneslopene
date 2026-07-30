9f59ce9cc4b3c7ac6483d667e8cf8b801d69cd02
Emilie Nilsen <37487484+emilienilsen@users.noreply.github.com>
2026-07-30 20:14:46 +0200

    Oppdatert tekst på navngitte punkter (#412)
    * Oppdatert tekst på navngitte punkter
    
    * Oppdatert tekst på liansvingen og hundebakken
    
    * Skriveleif
    
    * Skriveleif
    
    ---------
    
    Co-authored-by: Emilie <eilie.nilsen@vegvesen.no>

--- frontend/src/data/loypekartData.ts
+++ frontend/src/data/loypekartData.ts
@@ -37,7 +37,7 @@ export const pins: Pin[] = [
     category: "poi",
     distance: "360 m",
     description:
-      "Dette er navnet på svingen som kommer etter 360 meter, der vi løper til høyre ut av Lundestadveien. Den har fått sitt navn etter Raymond Westberg. Raymond er en jovial og sympatisk løper, og blant de med flest løp i Torsdagsløpets over 40-årige historie. Siden Raymond liker å løpe raskt fra start har han ofte vært blant de første inn i denne svingen.",
+      "Dette er navnet på svingen som kommer etter 360 meter, der vi løper til høyre ut av Lundestadveien. Den har fått sitt navn etter Raymond Westberg. Raymond er en jovial og sympatisk løper, og blant de med flest løp i Torsdagsløpets over 45-årige historie. Siden Raymond liker å løpe raskt fra start har han ofte vært blant de første inn i denne svingen.",
     photo: {
       fileName: "raymondsvingen.webp",
       fallback:
@@ -52,7 +52,7 @@ export const pins: Pin[] = [
     category: "poi",
     distance: "1 - 1,4 km",
     description:
-      "Strekningen fra 1 km-merket og deretter ca. 370 meter kaller vi nå Sletnersletta. Den har fått navn etter Kalnes-lærer og torsdagsløp-ildsjel Dag Inge Sletner. Han har gjort en formidabel innsats for å holde den blå løypa framkommelig, og høsten 2019 gravde han langt og lenge for å lede vannet vekk rett etter 1 km-merket. Dag løper også gjerne Torsdagsløpet, og gjør han ikke det hjelper han gjerne til med arrangementet.",
+      "Strekningen fra 1 km-merket og deretter ca. 370 meter kalles Sletnersletta. Den har fått navn etter Dag Inge Sletner som tidligere har gjort en formidabel innsats for å holde den blå løypa framkommelig. Høsten 2019 gravde han langt og lenge for å lede vannet vekk rett etter 1 km-merket. Dag løper også gjerne Torsdagsløpet.",
     photo: {
       fileName: "sletnersletta.webp",
       fallback:
@@ -67,7 +67,7 @@ export const pins: Pin[] = [
     category: "poi",
     distance: "ca. 1,55 km",
     description:
-      "Etter ca. 1550 meter av løypa kommer vi til en liten forhøyning der det er ganske bratt på begge sider. Fra denne toppen er det god mulighet til å sette fart nedover. En av dem som liker å gjøre det er Alexander Baustad, en hyggelig og relativt ung torsdagsløper. Han har fått æren av å gi navn til det vi har valgt å kalle en kolle, altså Baustadkollen.",
+      "Etter ca. 1550 meter av løypa kommer vi til en liten forhøyning der det er ganske bratt på begge sider. Fra denne toppen er det god mulighet til å sette fart nedover. En av dem som liker å gjøre det er Alexander Baustad, en veldig hyggelig torsdagsløper. Han har fått æren av å gi navn til det vi har valgt å kalle en kolle, altså Baustadkollen.",
     photo: {
       fileName: "baustadkollen.webp",
       fallback:
@@ -82,7 +82,7 @@ export const pins: Pin[] = [
     category: "poi",
     distance: "1,6 - 1,7 km",
     description:
-      "Like etter Baustadkollen, vi er nå på mellom 1600 og 1700 meter, kommer vi til et parti der det for en tid tilbake kunne være vanskelig å komme seg trygt og greit gjennom. Det var nødvendig både å legge på grus og gjøre andre grep for å lede vannet vekk. Hovedmannen bak dette er Svein Bergerud. Svein gjør en formidabel jobb med å forbedre og vedlikeholde de ulike løypene i Kalnesskogen. Han har dessuten vært med i samtlige torsdagsløp i perioden 2011-2019. Det passer fint at han gir navn til denne strekningen, som er like før den blå og den gule løypa skiller lag.",
+      "Like etter Baustadkollen, vi er nå på mellom 1600 og 1700 meter, kommer vi til et parti der det tidligere kunne være vanskelig å komme seg trygt og greit gjennom. Det var nødvendig både å legge på grus og gjøre andre grep for å lede vannet vekk. Hovedmannen bak dette er Svein Bergerud. Svein gjør en formidabel jobb med å forbedre og vedlikeholde de ulike løypene i Kalnesskogen. Han har dessuten vært med i samtlige torsdagsløp i perioden 2011-2019, og har deltatt i så å si hvert eneste løp etter dette. Det passer fint at han gir navn til denne strekningen, som er like før den blå og den gule løypa skiller lag.",
     photo: {
       fileName: "bergerudpassasjen.webp",
       fallback:
@@ -97,7 +97,7 @@ export const pins: Pin[] = [
     category: "poi",
     distance: "1,72 - 1,92 km",
     description:
-      "Strekningen fra 1720 meter til 1920 meter er kanskje den aller flotteste delen av den blåmerkede løypa. Du har ganske tett skog rundt deg, med en myk skogsbunn beina dine kan kose seg med, og akkurat passe med røtter. Denne strekningen har vi valgt å gi navn etter Bjørn Paulsrud, en av Torsdagsløpets grunnleggere og trolig den viktigste personen i løpets historie. Han var ikke bare en løpsorganisator, men hadde også stor omsorg for løperne.",
+      "Strekningen fra 1720 meter til 1920 meter er kanskje den aller flotteste delen av den blåmerkede løypa. Du har ganske tett skog rundt deg, med en myk skogbunn beina dine kan kose seg med, og akkurat passe med røtter. Denne strekningen har vi valgt å gi navn etter Bjørn Paulsrud, en av Torsdagsløpets grunnleggere og trolig den viktigste personen i løpets historie. Han var ikke bare en løpsorganisator, men hadde også stor omsorg for løperne.",
     photo: {
       fileName: "paulsrudholtet.webp",
       fallback:
@@ -112,7 +112,7 @@ export const pins: Pin[] = [
     category: "poi",
     distance: "ca. 1,92 km",
     description:
-      "Etter 1920 meter i løypa svinger vi omtrent 90 grader til venstre, og løper 20 meter oppover noe som kan minne om en trapp. Denne trappa har fått navn etter mannen som har løpt over 1000 torsdagsløp, nemlig Tom Sognlien. Synes du det er litt tungt å løpe opp denne stigningen kan du jo tenke på at det er fullt mulig å gjøre det over 1000 ganger.",
+      "Etter 1920 meter i løypa svinger vi omtrent 90 grader til venstre, og løper 20 meter oppover noe som kan minne om en trapp. Denne trappa har fått navn etter mannen som har flest torsdagsløp av alle, nemlig Tom Sognlien. Synes du det er litt tungt å løpe opp denne stigningen kan du jo tenke på at det er fullt mulig å gjøre nærmere 1000 ganger.",
     photo: {
       fileName: "tomtrappa.webp",
       fallback:
@@ -127,7 +127,7 @@ export const pins: Pin[] = [
     category: "poi",
     distance: "ca. 2,4 km",
     description:
-      "En av de få stedene i løypa som lenge har hatt et allment kjent navn blant løperne. Et godt referansepunkt halvveis ut i skogen.",
+      "Etter 2400 meter gjør løypa en 90-graders sving til venstre. Den har fått navnet Liansvingen etter løperen Svein Lian, som en gang tok til venstre litt for tidlig og dermed kuttet svingen.\n",
     photo: {
       fileName: "liansvingen.webp",
       fallback:
@@ -142,7 +142,7 @@ export const pins: Pin[] = [
     category: "poi",
     distance: "ca. 2,9 km",
     description:
-      "Etter ca. 2,9 km i løypa er vi på en topp med flott utsikt, som vi har altfor lite tid til å nyte når vi løper Torsdagsløpet. Legg derfor turen innom her ved en annen anledning, gjerne når solen er i ferd med å stå opp. Denne toppen har nå fått navn etter Bjørn Paulsruds arvtaker og nåværende leder for Torsdagsløpet, Per Prøitz. Den tidligere habile løperen har de siste årene gjort en formidabel jobb som arrangør og tidtaker, og det er alltid hyggelig å komme til Torsdagsløpet når Per står ved startstreken.",
+      "Etter ca. 2,9 km i løypa er vi på en topp med flott utsikt, som vi har altfor lite tid til å nyte når vi løper Torsdagsløpet. Legg derfor turen innom her ved en annen anledning, gjerne når solen er i ferd med å stå opp. Denne toppen har fått navn etter Bjørn Paulsruds arvtaker og nåværende leder for Torsdagsløpet, Per Prøitz. Den tidligere habile løperen har de siste årene gjort en formidabel jobb som arrangør og tidtaker, og det er alltid hyggelig å komme til Torsdagsløpet når Per står ved startstreken.",
     photo: {
       fileName: "proitztoppen.webp",
       fallback:
@@ -157,7 +157,7 @@ export const pins: Pin[] = [
     category: "poi",
     distance: "ca. 3,4 km",
     description:
-      "Et av de mest kjente stedene i løypa, og det med lengst tradisjon for navn blant løperne. En krevende bakke som tester bena sent i løypa.",
+      "Hundebakken er et av de mest kjente stedene i torsdagsløypa, og et parti mange kjenner godt i beina. Bakken starter ved 3400 meter og er 130 meter lang, målt til der en annen sti går til venstre. Den har fått sitt navn etter en hund som pleide å bjeffe når løperne kom opp bakken.",
     tips: "Du er over halvveis - gi det du har igjen!",
     photo: {
       fileName: "hundebakken.webp",
@@ -173,7 +173,7 @@ export const pins: Pin[] = [
     category: "poi",
     distance: "4,27 - 4,44 km",
     description:
-      "Fra 4270 til 4440 meter i den blå løypa løper vi oppover en smal sti før vi kommer ut på Lundestadveien og skal spurte mot mål. Denne stien er en trivelig del av løypa, men på dette stadiet av løpet kjenner vi gjerne at kroppen er sliten og at vi trenger en oppmuntring. Derfor passer det bra å gi denne strekningen, som vi kan kalle en kneik, navn etter Helge Rosnes. Den trofaste torsdagsløperen er alltid oppmuntrende og støttende overfor sine løpekolleger, noe mange setter stor pris på.",
+      "Fra 4270 til 4440 meter i den blå løypa løper vi oppover en smal sti før vi kommer ut på Lundestadveien og skal spurte mot mål. Denne stien er en trivelig del av løypa, men på dette stadiet av løpet kjenner vi gjerne at kroppen er sliten og at vi trenger en oppmuntring. Derfor passer det bra å gi denne strekningen, som vi kan kalle en kneik, navn etter Helge Rosnes. Den tidligere og trofaste torsdagsløperen var alltid oppmuntrende og støttende overfor sine løpekolleger, noe mange satte stor pris på.",
     photo: {
       fileName: "helgekneika.webp",
       fallback:

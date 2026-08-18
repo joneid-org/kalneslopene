c4b371c308a8219b2a1b32405cb2683eecaec64b
ejoneid <eirijomine@hotmail.com>
2026-08-18 18:19:50 +0200

    chore: migrate domain

--- infra/dns.tf
+++ infra/dns.tf
@@ -1,10 +1,23 @@
 resource "hcloud_zone" "torsdagslopet" {
   name = var.domain
   mode = "primary"
-  ttl  = 3600
+  ttl  = 300
 }
 
 resource "hcloud_zone_rrset" "wildcard_a" {
+  zone = hcloud_zone.torsdagslopet.name
+  name = "*"
+  type = "A"
+  records = [{ value = hcloud_server.dokploy.ipv4_address }]
+}
+
+resource "hcloud_zone" "torsdagslopet_old" {
+  name = var.domain_old
+  mode = "primary"
+  ttl  = 3600
+}
+
+resource "hcloud_zone_rrset" "wildcard_a_old" {
   zone = hcloud_zone.torsdagslopet.name
   name = "*"
   type = "A"

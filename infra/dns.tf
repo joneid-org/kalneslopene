b4ddfbca1f85084f8efb2fcc1e6e30696500f9a1
ejoneid <eirijomine@hotmail.com>
2026-08-20 15:11:42 +0200

    chore: add root dns record

--- infra/dns.tf
+++ infra/dns.tf
@@ -4,20 +4,14 @@ resource "hcloud_zone" "torsdagslopet" {
   ttl  = 300
 }
 
-resource "hcloud_zone_rrset" "wildcard_a" {
+resource "hcloud_zone_rrset" "root_a" {
   zone = hcloud_zone.torsdagslopet.name
-  name = "*"
+  name = "@"
   type = "A"
   records = [{ value = hcloud_server.dokploy.ipv4_address }]
 }
 
-resource "hcloud_zone" "torsdagslopet_old" {
-  name = var.domain_old
-  mode = "primary"
-  ttl  = 3600
-}
-
-resource "hcloud_zone_rrset" "wildcard_a_old" {
+resource "hcloud_zone_rrset" "wildcard_a" {
   zone = hcloud_zone.torsdagslopet.name
   name = "*"
   type = "A"

0eef3fcf40ecd7e8f8f3dd3a7ce5b67be2b6832d
ejoneid <eirijomine@hotmail.com>
2026-08-23 23:16:35 +0200

    --wip-- [skip ci]

--- infra/dns.tf
+++ infra/dns.tf
@@ -1,19 +1,55 @@
 resource "hcloud_zone" "torsdagslopet" {
   name = var.domain
   mode = "primary"
-  ttl  = 300
+  ttl  = 300 # 86400
 }
 
 resource "hcloud_zone_rrset" "root_a" {
-  zone = hcloud_zone.torsdagslopet.name
-  name = "@"
-  type = "A"
+  zone    = hcloud_zone.torsdagslopet.name
+  name    = "@"
+  type    = "A"
   records = [{ value = hcloud_server.dokploy.ipv4_address }]
 }
 
 resource "hcloud_zone_rrset" "wildcard_a" {
-  zone = hcloud_zone.torsdagslopet.name
-  name = "*"
-  type = "A"
+  zone    = hcloud_zone.torsdagslopet.name
+  name    = "*"
+  type    = "A"
   records = [{ value = hcloud_server.dokploy.ipv4_address }]
+}
+
+# E-posten ligger fortsatt hos one.com, så MX/SPF/DKIM må settes eksplisitt her etter at NS ble flyttet til Hetzner.
+resource "hcloud_zone_rrset" "root_mx" {
+  count = length(var.mail_mx_records) > 0 ? 1 : 0
+
+  zone = hcloud_zone.torsdagslopet.name
+  name = "@"
+  type = "MX"
+  records = [
+    for host, priority in var.mail_mx_records : { value = "${priority} ${trimsuffix(host, ".")}." }
+  ]
+}
+
+resource "hcloud_zone_rrset" "root_txt" {
+  count = length(local.root_txt_records) > 0 ? 1 : 0
+
+  zone = hcloud_zone.torsdagslopet.name
+  name = "@"
+  type = "TXT"
+  records = [
+    for value in local.root_txt_records : { value = provider::hcloud::txt_record(value) }
+  ]
+}
+
+resource "hcloud_zone_rrset" "dkim_cname" {
+  for_each = var.mail_dkim_cnames
+
+  zone    = hcloud_zone.torsdagslopet.name
+  name    = each.key
+  type    = "CNAME"
+  records = [{ value = "${trimsuffix(each.value, ".")}." }]
+}
+
+locals {
+  root_txt_records = compact(concat([var.mail_spf_record], var.extra_root_txt_records))
 }
--- infra/variables.tf
+++ infra/variables.tf
@@ -15,10 +15,34 @@ variable "os_type" {
 
 variable "ssh_public_keys" {
   description = "Map of team mebers name -> public ssh key"
-  type = map(string)
+  type        = map(string)
 }
 
 variable "environments" {
   description = "Map of environments -> url"
-  type = map(string)
+  type        = map(string)
+}
+
+variable "mail_mx_records" {
+  description = "MX-verter -> prioritet for sonerota. Hent eksakte verdier fra one.com: Avanserte innstillinger -> DNS-innstillinger."
+  type        = map(number)
+  default     = {}
+}
+
+variable "mail_spf_record" {
+  description = "SPF-record på sonerota. Tom streng slår den av."
+  type        = string
+  default     = "v=spf1 include:_custspf.one.com ~all"
+}
+
+variable "extra_root_txt_records" {
+  description = "Flere TXT-records på sonerota (domeneverifisering, DMARC-lignende oppsett o.l.)"
+  type        = list(string)
+  default     = []
+}
+
+variable "mail_dkim_cnames" {
+  description = "DKIM CNAME-navn -> mål. one.com support gir ut fire av disse når DNS ligger utenfor one.com."
+  type        = map(string)
+  default     = {}
 }
